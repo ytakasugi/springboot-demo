45a2026a94b724b1a16e7ddc7570bc0007dde060
ytakasugi <sh7.tibi0129@gmail.com>
2022-08-23 23:41:05 +0900

    change file

---
+++ demo/src/main/java/jp/co/checkbord/demo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package jp.co.checkbord.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
+    }
+}
